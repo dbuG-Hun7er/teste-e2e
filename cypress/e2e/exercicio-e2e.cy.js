94eab3decc284f79d6ba5b965217405fff4067b1
dbuG-Hun7er <dry_lucas@outlook.com>
2025-02-06 14:58:18 -0300

    teste de compra de prudotos na loja EbacShop

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+import protudosPage from "../support/page_objects/protudos.page";
+
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
       Quero acessar a Loja EBAC 
@@ -10,13 +12,33 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       E validando minha compra ao final */
 
   beforeEach(() => {
-      cy.visit('/')
+      protudosPage.visitUrl();
   });
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
+     cy.fixture('produtos').then((dados) => {
+    protudosPage.buscarProduto(dados[1].nomeProduto)
+    protudosPage.addProdutoCarrinho(
+        dados[1].tamanho,
+        dados[1].cor,
+        dados[1].quantidade
+    )
+    cy.get('.woocommerce-message > .button').click()
+    cy.get('.checkout-button').click()
+    cy.get('.showlogin').click()
+    
+    cy.fixture("perfil").then((dados) => {
+    cy.get('#username').type(dados.usuario)
+    cy.get('#password').type(dados.senha)
+    cy.get('.woocommerce-button').click()
       
+    });
+    cy.get('#payment_method_cod').click()
+    cy.get('#terms').click()
+    cy.get('#place_order').click()
+    cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
   });
 
 
-})
+  });
+});
---
+++ cypress/support/page_objects/protudos.page.js
@@ -0,0 +1,16 @@
+class produtoPage {
+    visitUrl() {
+        cy.visit('produtos')
+    }
+    buscarProduto(nomeProduto) {
+        cy.get('[type="text"]').eq(1).type(nomeProduto)
+        cy.get('.button-search').eq(1).click()
+    }
+    addProdutoCarrinho(tamanho, cor, quantidade) {
+        cy.get('.button-variable-item-' + tamanho).click()
+        cy.get('.button-variable-item-' + cor).click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+    }
+}
+export default new produtoPage();
