75d71487b37126ce2aad960455fccbbf48c22de1
dbuG-Hun7er <dry_lucas@outlook.com>
2025-02-12 00:29:02 -0300

    add Video

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,12 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://lojaebac.ebaconline.art.br/"
+    baseUrl: "http://lojaebac.ebaconline.art.br/",
+    video: true,
+    reporter: 'junit',
+    reporterOptions: {
+    mochaFile: 'results/my-test-output.xml',
+    toConsole: true
+  }
   },
 });
